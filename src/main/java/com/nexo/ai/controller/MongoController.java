1eb4bf879ff310be90842de4b155609aa61f6979
LAPTOP-AM598OQO\Nexo <camila@nexo.ai>
2018-05-28 15:09:58 -0300

    corretion controller

--- src/main/java/com/nexo/ai/controller/MongoController.java
+++ src/main/java/com/nexo/ai/controller/MongoController.java
@@ -1,14 +1,18 @@
 package com.nexo.ai.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.nexo.ai.model.Example;
 import com.nexo.ai.service.MongoService;
 
+@CrossOrigin
+@RestController
 public class MongoController {
 
 	@Autowired
